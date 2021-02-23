9bf16c76783be945ab764495c3dccfb94e154514
Christian LeVesque <christian@christianlevesque.io>
2021-02-23 15:23:43 -0600

    updated footer

--- frontend/src/components/footer/Footer.tsx
+++ frontend/src/components/footer/Footer.tsx
@@ -7,10 +7,10 @@ const Footer: React.FunctionComponent = (): JSX.Element => {
 	return (
 		<footer className="app__footer footer">
 			<p className="footer__paragraph">
-				&copy; {new Date().getFullYear()} Bastion of Shadows. All rights reserved.
+				&copy; {new Date().getFullYear()} Bastion of Shadows. All rights reserved. The names of places, creatures, technologies, etc. are trademarks of Lucasfilm Ltd. and The Walt Disney Company. Bastion of Shadows does not own or lease the rights to these trademarks. Bastion of Shadows uses all trademarks in association with Fair Use Doctrine in the United States.
 			</p>
 			<p className="footer__paragraph">
-				See our statement on <NavLink to="/fair-use">Fair Use of trademark material</NavLink>.
+				See our statement on the <NavLink to="/fair-use">Fair Use of intellectual property</NavLink>.
 			</p>
 		</footer>
 	)
